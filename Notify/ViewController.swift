e174531e282c7847215f5d2236d4de4f1d13cdef
Gappmob Macbook <Gappmob@Gappmobs-MacBook-Pro.local>
2015-09-22 15:03:45 +0530

    Initial Commit

---
+++ Notify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notify
+//
+//  Created by Gappmob Macbook on 22/09/15.
+//  Copyright (c) 2015 Gappmob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
